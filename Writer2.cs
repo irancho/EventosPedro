6693468a27d1598e68df7f170657b7b0f9b6d3ed
Pedro Irán Méndez Pérez <pmendez@sti-mx.com>
2020-01-13 17:34:31 -0600

    Segunda Versión

---
+++ Writer2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AplicacionEventos.Writing
+{
+    public class Writer : IWriter
+    {
+        public void WriteResult(string cMessage)
+        {
+            Console.WriteLine(cMessage);
+        }
+    }
+}
